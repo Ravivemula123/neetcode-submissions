867011cd498c53450aeb919b20446403bfce3457
Ravivemula123 <106158352+Ravivemula123@users.noreply.github.com>
2026-06-16 21:32:15 +0530

    Add: score-of-a-string - submission-0

---
+++ Data Structures & Algorithms/score-of-a-string/submission-0.py
@@ -0,0 +1,26 @@
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        ascii_values ={"a":97,"b":98,"c":99,"d":100,"e":101,"f":102,
+        "g":103,"h":104,"i":105,"j":106,"k":107,"l":108,
+        "m":109,"n":110,"o":111,"p":112,"q":113,"r":114,
+        "s":115,"t":116,"u":117,"v":118,"w":119,"x":120,
+        "y":121,"z":122}
+
+        array = []
+        for info in s:
+            value = ascii_values.get(info)
+            array.append(value)
+
+        
+        result = []
+
+        for index in range(len(array)-1):
+            if not array[index-1]:
+                break
+            sum_value = array[index+1] -array[index]
+            result.append(abs(sum_value))
+
+
+        return sum(result)
+
+    
